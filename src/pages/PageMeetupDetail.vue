ba6d0c1b08db74fbdb2b8d55a9183ec9834d1023
Cyvision <cyvision13@outlook.com>
2020-06-01 23:24:10 +0430

    Router Navigation to Detail Page

---
+++ src/pages/PageMeetupDetail.vue
@@ -0,0 +1,18 @@
+<template>
+    
+    <h1>I am Nothing</h1>
+    
+</template>
+
+<script>
+export default {
+    created(){
+        debugger
+        const meetupId= this.$route.params.id
+    }
+}
+</script>
+
+<style  scoped>
+
+</style>
