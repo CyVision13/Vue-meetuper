71ce805bb6711249e9e561d6e6dc2ba21d77b88b
Cyvision <cyvision13@outlook.com>
2020-06-08 15:34:38 +0430

    Not Found Page.

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+      <ErrorView :title="'Ooooops, Page you are Trying to access doesn\'t exist'"
+                 :status="'404'" 
+                 :navigateToPage="'PageHome'" 
+                 :navigateToText="'Navigate Back Home'">
+
+      </ErrorView>
+  </div>
+</template>
+
+<script>
+    import ErrorView from    '@/components/ErrorView'
+  export default {
+      components:{
+          ErrorView
+      }
+  }
+</script>
+
+<style scoped>
+  
+</style>
