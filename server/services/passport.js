a4546a3340d9ccc7b69778494c69c566795066b2
Cyvision <cyvision13@outlook.com>
2020-08-12 09:59:16 +0430

    Passport intro fkn hard :|

--- server/services/passport.js
+++ server/services/passport.js
@@ -18,5 +18,13 @@ passport.use(new LocalStrategy({
 },(email,password,done)=>{
     User.findOne({email},(err,user)=>{
         if(err){return done(err)}
+        if(!user){return done(null,false)}
+
+        user.comparePassword(password,function(err,isMatch){
+            if(err){return done(err);}
+            if(!isMatch){return done(null,false)}
+
+            return done(null,user)
+        })
     })
 }))
