0e928fcc76ec699e9d8ca620629d1812b9527c82
Cyvision <cyvision13@outlook.com>
2020-09-07 20:55:45 +0430

    err Fixed

--- src/router/index.js
+++ src/router/index.js
@@ -1,82 +1,77 @@
-import Vue from 'vue';
-import Router from 'vue-router'
-import store from   '@/store'
+import Vue from "vue";
+import Router from "vue-router";
+import store from "@/store";
 
-import PageHome from '@/pages/PageHome'
-import PageMeetupDetail from '@/pages/PageMeetupDetail'
-import PageMeetupFind from '@/pages/PageMeetupFind'
-import PageNotFound from '@/pages/PageNotFound'
-import PageRegister from '@/pages/PageRegister'
-import PageLogin from '@/pages/PageLogin'
-import PageSecret from '@/pages/PageSecret'
-import PageNotAuthenticated from '@/pages/PageNotAuthenticated'
+import PageHome from "@/pages/PageHome";
+import PageMeetupDetail from "@/pages/PageMeetupDetail";
+import PageMeetupFind from "@/pages/PageMeetupFind";
+import PageNotFound from "@/pages/PageNotFound";
+import PageRegister from "@/pages/PageRegister";
+import PageLogin from "@/pages/PageLogin";
+import PageSecret from "@/pages/PageSecret";
+import PageNotAuthenticated from "@/pages/PageNotAuthenticated";
 
-
-Vue.use(Router)
+Vue.use(Router);
 
 const router = new Router({
-    routes:[
-        {
-            path:'/',
-            name:'PageHome',
-            component:PageHome
-        },
-        {
-            path:'/meetups/secret',
-            name:'PageSecret',
-            component:PageSecret,
-            meta:{onlyAuthUser:true}
-            
-        },
-        {
-            path:'/meetups/:id',
-            name:'PageMeetupDetail',
-            component:PageMeetupDetail
-        },
-        {
-            path:'/find',
-            name:'PageMeetupFind',
-            component:PageMeetupFind
-        },
-        {
-            path:'/register',
-            name:'PageRegister',
-            component:PageRegister
-        },
-        {
-            path:'/login',
-            name:'PageLogin',
-            component:PageLogin
-        },
-        {
-            path:'/401',
-            name:'PageNotAuthenticated',
-            component:PageNotAuthenticated
-        },
-        {
-            path:'*',
-            name:'PageNotFound',
-            component:PageNotFound
-        }
-    ],
-    mode:'history'
-})
+  routes: [
+    {
+      path: "/",
+      name: "PageHome",
+      component: PageHome
+    },
+    {
+      path: "/meetups/secret",
+      name: "PageSecret",
+      component: PageSecret,
+      meta: { onlyAuthUser: true }
+    },
+    {
+      path: "/meetups/:id",
+      name: "PageMeetupDetail",
+      component: PageMeetupDetail
+    },
+    {
+      path: "/find",
+      name: "PageMeetupFind",
+      component: PageMeetupFind
+    },
+    {
+      path: "/register",
+      name: "PageRegister",
+      component: PageRegister
+    },
+    {
+      path: "/login",
+      name: "PageLogin",
+      component: PageLogin
+    },
+    {
+      path: "/401",
+      name: "PageNotAuthenticated",
+      component: PageNotAuthenticated
+    },
+    {
+      path: "*",
+      name: "PageNotFound",
+      component: PageNotFound
+    }
+  ],
+  mode: "history"
+});
 
+router.beforeEach((to, from, next) => {
+  store.dispatch("auth/getAuthUser").then(authUser => {
+    console.log(authUser);
+    if (to.meta.onlyAuthUser) {
+        // if(store.getters["auth/isAuthenticated"]){ 
+      if (authUser) {
+        next();
+      } else {
+        next({ name: "PageNotAuthenticated" });
+      }
+    } else next();
+  });
+});
 
-router.beforeEach((to,from,next)=>{
-    store.dispatch('auth/getAuthUser')
-        .then(authUser=>{
-            if(to.meta.onlyAuthUser){
-                if(store.getters['auth/isAuthenticated']){
-                    console.log('done');
-                    next()
-                }else {
-                    next({name:'PageNotAuthenticated'})
-                }
-            }else  
-              next();   
-          
-        })
-})
-
-export default router
+export default router;
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,66 +1,66 @@
-import axios from 'axios'
-import { auth } from 'firebase-admin'
+import axios from "axios";
+// import { auth } from 'firebase-admin'
 export default {
-    namespaced:true,
-    state:{
-        user:null,
-        isAuthResolved:false
+  namespaced: true,
+  state: {
+    user: null,
+    isAuthResolved: false
+  },
+  getters: {
+    authUser(state) {
+      return state.user || null;
     },
-    getters:{
-        authUser(state){
-            return state.user || null
-        },
-        isAuthenticated(state){
-            return !!state.user
-        }
+    isAuthenticated(state) {
+      return !!state.user;
     }
-    ,
-    actions:{
-        loginWithEmailPassword({commit},userData){
-            return axios.post('/api/v1/users/login',userData)
-                .then(res=>{
-                    const user = res.data
-                    commit('setAuthUser',user)
-                })
-        },
-        registerUser(context,userData){
-            return axios.post('/api/v1/users/register',userData)
-        },
-        logout({commit}){
-            return axios.post('/api/v1/users/logout')
-                .then((res)=>{
-                    commit('setAuthUser',null)
-                    return true
-                })
-                .catch(err=>{
-                    console.log(err);
-                })
-        },
-        getAuthUser({commit,getters}){
-            const authUser = getters['authUser']
-            if(authUser) { return Promise.resolve(authUser)}
-            return axios.get('api/v1/users/me')
-                .then(res=>{
-                    const user = res.data
-                    commit('setAuthUser',user)
-                    commit('setAuthState',true)
-                    return user
-                })
-                .catch(err =>{
-                    console.log(err);
-                    commit('setAuthUser',null)
-                    commit('setAuthState',true)
-                    return undefined
-                })
-        }
-        
+  },
+  actions: {
+    loginWithEmailPassword({ commit }, userData) {
+      return axios.post("/api/v1/users/login", userData).then(res => {
+        const user = res.data;
+        commit("setAuthUser", user);
+      });
     },
-    mutations:{
-        setAuthUser(state,user){
-            return state.user = user
-        },
-        setAuthState(state,authState){
-            return state.isAuthResolved = authState
-        }
+    registerUser(context, userData) {
+      return axios.post("/api/v1/users/register", userData);
+    },
+    logout({ commit }) {
+      return axios
+        .post("/api/v1/users/logout")
+        .then(() => {
+          commit("setAuthUser", null);
+          return true;
+        })
+        .catch(err => {
+          return err;
+        });
+    },
+    getAuthUser({ commit, getters }) {
+      const authUser = getters["authUser"];
+      if (authUser) {
+        return Promise.resolve(authUser);
+      }
+      return axios
+        .get("api/v1/users/me")
+        .then(res => {
+          const user = res.data;
+          commit("setAuthUser", user);
+          commit("setAuthState", true);
+          return user;
+        })
+        .catch(err => {
+          commit("setAuthUser", null);
+          commit("setAuthState", true);
+          return err;
+        });
     }
-}
+  },
+  mutations: {
+    setAuthUser(state, user) {
+      state.user = user;
+    },
+    setAuthState(state, authState) {
+      return (state.isAuthResolved = authState);
+    }
+  }
+};
