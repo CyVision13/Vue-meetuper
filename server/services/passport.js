dd7ead71eb740717c90cc0f5c37fb339d848bcb4
Cyvision <cyvision13@outlook.com>
2020-09-09 12:01:54 +0430

    Server Init (half)

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,30 +1,46 @@
-const passport = require('passport')
-const LocalStrategy = require('passport-local')
-const User = require('../models/users')
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("../models/users");
 
+const config = require("../config/dev");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 
-passport.serializeUser(function(user,done){
-    done(null,user.id)
-})
-passport.deserializeUser(function(id,done){
-    User.findById(id,function(err,user){
-        done(err,user)
-    })
-})
+// passport.serializeUser(function(user,done){
+//     done(null,user.id)
+// })
+// passport.deserializeUser(function(id,done){
+//     User.findById(id,function(err,user){
+//         done(err,user)
+//     })
+// })
 
-passport.use(new LocalStrategy({
-    usernameField: 'email',
-    passwordField:'password'
-},(email,password,done)=>{
-    User.findOne({email},(err,user)=>{
-        if(err){return done(err)}
-        if(!user){return done(null,false)}
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password"
+    },
+    (email, password, done) => {
+      User.findOne({ email }, (err, user) => {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false);
+        }
 
-        user.comparePassword(password,function(err,isMatch){
-            if(err){return done(err);}
-            if(!isMatch){return done(null,false)}
+        user.comparePassword(password, function(err, isMatch) {
+          if (err) {
+            return done(err);
+          }
+          if (!isMatch) {
+            return done(null, false);
+          }
 
-            return done(null,user)
-        })
-    })
-}))
+          return done(null, user);
+        });
+      });
+    }
+  )
+);
