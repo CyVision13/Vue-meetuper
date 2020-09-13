8cd6cc0fb4383f295164fbeb5ea4edda2e44e481
Cyvision <cyvision13@outlook.com>
2020-09-13 13:52:03 +0430

    Meetup Create Init (Create Meetup Btn And Router-link)

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -7,7 +7,7 @@
       <div class="m-b-lg">
         <h1 class="title is-inline">Featured Meetups in "Location"</h1>
         <AppDropdown />
-        <button class="button is-primary is-pulled-right m-r-sm">Create Meetups</button>
+        <router-link v-if="user" :to="{name : 'PageMeetupCreate'}" class="button is-primary is-pulled-right m-r-sm">Create Meetups</router-link>
         <router-link :to="{name : 'PageMeetupFind'}" class="button is-primary is-pulled-right m-r-sm">All</router-link>
         <!-- <h1>{{someOtherGetter}}</h1> -->
       </div>
@@ -58,6 +58,9 @@
    
     mixins:[pageLoader],
     computed:{
+        ...mapGetters({
+          'user':'auth/authUser'
+        }),
         ...mapState({
         meetups:(state)=> state.meetups.items,
         categories:(state)=> state.categories.items
