8502795252f1cdbe19dbd90f9a56b60185446877
Cyvision <cyvision13@outlook.com>
2020-09-29 10:23:01 +0330

    Thread list & Post Create Components 50%

--- src/pages/PageMeetupDetail.vue
+++ src/pages/PageMeetupDetail.vue
@@ -111,47 +111,9 @@
                />
             </div>
 
-            <div class="content is-medium">
-              <h3 class="title is-3">Threads</h3>
-              <div v-for="thread in orderThreads" :key="thread._id" class="box">
-                <h4 id="const" class="title is-3">{{ thread.title }}</h4>
-                <form class="post-craete">
-                  <div class="field">
-                    <textarea
-                      placeholder="Write a post"
-                      class="textarea textarea-post"
-                    ></textarea>
-                    <button :disabled="true" class="button is-primary m-t-tsm">
-                      Send
-                    </button>
-                  </div>
-                </form>
+            <ThreadList :threads="orderThreads" />
 
-                <article
-                  v-for="post in thread.posts"
-                  :key="post._id"
-                  class="media post-item"
-                >
-                  <figure class="media-left is-rounded user-image">
-                    <p class="image is-32x32">
-                      <img :src="post.user.avatar" alt="" class="is-rounded" />
-                    </p>
-                  </figure>
-                  <div class="media-content">
-                    <div class="content is-medium">
-                      <div class="post-content">
-                        <strong class="author">{{ post.user.name }}</strong>
-                        <small class="post-time">{{
-                          post.updatedAt | formatTime("LLL")
-                        }}</small>
-                        <br />
-                        <p class="post-content-message">{{ post.text }}</p>
-                      </div>
-                    </div>
-                  </div>
-                </article>
-              </div>
-            </div>
+
           </div>
         </div>
       </div>
@@ -162,9 +124,11 @@
 <script>
 import ThreadCreateModal from "@/components/ThreadCreateModal";
 import { mapActions, mapState } from "vuex";
+import ThreadList from '@/components/threadList'
 export default {
   components: {
     ThreadCreateModal,
+    ThreadList
   },
   computed: {
     ...mapState({
@@ -322,40 +286,5 @@ li {
   margin-bottom: 15px;
 }
 
-.content {
-  figure {
-    margin-bottom: 0;
-  }
-}
 
-.media-content-threads {
-  background-color: #f1f1f1;
-  padding: 3px 20px;
-  border-radius: 10px;
-  margin-right: 40px;
-  width: 100px;
-}
-
-.media-left.user-image {
-  margin: 0;
-  margin-right: 15px;
-}
-.post-item {
-}
-.media + .media {
-  border: none;
-  margin-top: 0;
-}
-.post-content {
-  margin: 0;
-  &-message {
-    font-size: 16px;
-  }
-  .author {
-    font-size: 16px;
-  }
-  .post-time {
-    font-size: 16px;
-  }
-}
 </style>
