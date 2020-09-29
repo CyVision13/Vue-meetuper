bfd7145f0e125371421c12ba40f2008340efe2f4
Cyvision <cyvision13@outlook.com>
2020-09-29 16:14:16 +0330

    Auto Expand  Directives 100%

--- src/components/PostCreate.vue
+++ src/components/PostCreate.vue
@@ -1,32 +1,29 @@
 <template>
-<!-- <div> v-with-warning:red.prevent.yell="'what a nice day (:'"</div> -->
-     <form class="post-craete">
-                  <div class="field">
-                    <textarea
-                    
-                      placeholder="Write a post"
-                      class="textarea textarea-post"
-                      rows="1"
-                    ></textarea>
-                    <button :disabled="true" class="button is-primary m-t-tsm">
-                      Send
-                    </button>
-                  </div>
-                </form>
+  <!-- <div> v-with-warning:red.prevent.yell="'what a nice day (:'"</div> -->
+  <form class="post-craete">
+    <div class="field">
+      <textarea
+        v-auto-expand
+        placeholder="Write a post"
+        class="textarea textarea-post"
+        rows="1"
+      ></textarea>
+      <button :disabled="true" class="button is-primary m-t-tsm">Send</button>
+    </div>
+  </form>
 </template>
 <script>
-import withWarning from '@/directives/withWarning'
+// import withWarning from '@/directives/withWarning'
+import autoExpand from "@/directives/autoExpand";
 export default {
-    directives : {withWarning}
-}
+  directives: { autoExpand },
+};
 </script>
 <style lang="scss" scoped>
-
 .textarea-post {
   padding-bottom: 30px;
 }
 .post-craete {
   margin-bottom: 15px;
 }
-
 </style>
